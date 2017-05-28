6d3ab0ab9ec21fe05d575175deefc37e7b1bd2fb
prav583020644-8 <kanjana.supisara@gmail.com>
2017-05-29 00:44:40 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<?php $page="Music for you"; ?>
+<html lang="en">
+<head>
+ <meta charset="UTF-8"><title><?= $page ?></title>
+ <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body role="document">
+<div align="center"><marquee direction="left"><font color="black"><h1>MUSIC FOR YOU<h1></marquee></div></font> 
+<div class="container">
+<div class="col-xs-10 col-sm-2 col-md-2" style="color:white;background-color:yellow"><a href="hottmusic.php">เพลงใหม่มาแรง</a></div>
+<div class="col-xs-10 col-sm-2 col-md-2" style="color:white;background-color:pink"><a href="lovee.php">เพลงรัก</a></div>
+<div class="col-xs-10 col-sm-2 col-md-2" style="color:white;background-color:yellow"><a href="inter.php">เพลงสากล</a></div>
+<div class="col-xs-10 col-sm-2 col-md-2" style="color:white;background-color:pink"><a href="sad.php">เพลงเศร้า</a></div>
+<div class="col-xs-10 col-sm-2 col-md-2" style="color:white;background-color:yellow"><a href="dance.php">เพลงแดนซ์</a></div>
+<div class="col-xs-10 col-sm-2 col-md-2" style="color:white;background-color:pink"><a href="loginn.php">เข้าสู่ระบบ</a></div>
+
+<br>
+<br>
+<br>
+<br>
+
+	<table>
+	<tr>
+		<td><center><img src="https://www.img.in.th/images/9967349a08e2a53d5a1456357ae8f360.jpg" border="0" width="370" height="360">
+		<br><h2>เป็นทุกอย่าง</h2></center>
+		<br><font color="#6699FF">เป็นเพื่อนเธอไปดูหนัง
+                        อยู่ด้วยกันตอนเธอเหงา
+                        ฟังทุกเรื่องราว ที่เธอระบาย
+                          ยาวจนเช้า ฉันก็ยังยินดี
+                       แม้ไม่ใช่คนพิเศษ ไม่ได้สำคัญสำหรับเธอ</font>
+<a href = "every.php">ดูรายละเอียด</a></td> <br><br><br>
+		<td><center><img src="https://www.img.in.th/images/76ffe8a0922ba1d09505ac15d779b852.jpg" border="0" width="370" height="370">
+		<br><h2>ฉันไม่มี
+</h2></center>
+		<br><font color="#FF0033">เธอคนดี ฉันขอเวลาเธอสักหน่อย
+ก่อนที่เราจะไปต่อด้วยกัน
+เธอคนดี ให้ฉันได้พูดอะไรสักอย่าง
+ใช้เวลาแค่ไม่นาน</font>
+<a href = "not.php">ดูรายละเอียด</a</td>
+		<td><center><img src="https://www.img.in.th/images/7607fb967d9d508e96757831b84fdf0e.jpg" border="0" width="350" height="350">
+		<br><h2>หน้าหนาวที่แล้ว
+</h2></center>
+		<br><font color="#FFFF99">เด็กสมัยนี้โตไวเนอะ 
+เช้าลืมตาขึ้นมามองเธอในยามเช้าตรู่
+แอบหอมแก้มเธอเบาๆ ทั้งๆ ที่เธอไม่รู้
+ฝันกลางวันด้วยกัน ดอกไม้ของเธอกับฉัน
+แค่มีวันเหล่านั้นก็มีความสุขเหลือเกิน</font>
+<a href = "cool.php">ดูรายละเอียด</a</td>
+</tr>
+</table>
+<br>
+<BODY background="bg.jpg"></BODY>
+</div>
+
